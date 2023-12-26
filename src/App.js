7daefeb06ea1088d5d34f4b3067a7423b65c8033
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-12-26 23:10:08 +0100

    Global chart component complted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderInfos from './components/HeaderInfos';
+import GlobalChart from './components/GlobalChart';
+// import axios from 'axios';
+import {data} from './db.js';
 
 const App = () => {
-  return (
-    <div className='app-container'>
-      <header> 
-        <HeaderInfos />
-      </header>
-    </div>
-  );
+    const [coinsData, setCoinsData] = useState([]);
+
+    // useEffect(() => {
+    //    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y")
+    //         .then((res) => setCoinsData(res.data));
+    // }
+    // , []);
+
+    useEffect(() => {
+        setCoinsData(data);
+    }, []);
+
+    return (
+        <div className='app-container'>
+        <header> 
+            <HeaderInfos />
+            <GlobalChart coinsData={coinsData}/>
+        </header>
+        </div>
+    );
 };
 
 export default App;
